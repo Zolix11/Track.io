f09874fd74580697f51926369250e489ff72cf2a
FogtiIstvan <fogtiistvan47@gmail.com>
2022-10-15 12:11:49 +0200

    retrofit_https_client_done

--- Tesztek/Retrofit_test2/app/src/main/java/hu/bme/aut/retrofit_test2/MainActivity.kt
+++ Tesztek/Retrofit_test2/app/src/main/java/hu/bme/aut/retrofit_test2/MainActivity.kt
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import hu.bme.aut.retrofit_test2.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.lang.StringBuilder
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val BASE_URL = "http://23.97.188.188:80/"
+    val BASE_URL = "https://23.97.188.188:443/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +39,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getMyData(){
+        val okhttpclient = getUnsafeOkHttpClient()
         val retrofitBuilder = Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
+                .client(okhttpclient)
                 .build()
                 .create(ApiInterface::class.java)
         val retrofitData = retrofitBuilder.getData()
@@ -65,9 +72,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun postData(name: String, passw: String){
+        val okHttpClient = getUnsafeOkHttpClient()
         val retrofitBuilder = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .build()
             .create(ApiInterface::class.java)
         val retrofitData = retrofitBuilder.SendAndGetPost(name, passw)
@@ -87,5 +96,28 @@ class MainActivity : AppCompatActivity() {
                 Log.d("posthiba", "fetch fail: " + t.message)
             }
         })
+    }
+
+    private fun getUnsafeOkHttpClient(): OkHttpClient {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+            }
+
+            override fun getAcceptedIssuers() = arrayOf<X509Certificate>()
+        })
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory = sslContext.socketFactory
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }.build()
     }
 }
